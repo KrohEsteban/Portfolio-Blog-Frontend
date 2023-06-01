1072572532c0b78656e846820064cbac11efeda1
Esteban Kroh <krohesteban@gmail.com>
2023-06-01 11:56:32 -0300

    agregando amarillo a las barras de aptitudes

--- src/components/progresbar.tsx
+++ src/components/progresbar.tsx
@@ -13,12 +13,12 @@ export default function Progresbar(props:{stack:string, progress:number}):any {
         <div key={props.stack} className='space-y-4'>
         <h4 className='font-bold'>{props.stack}</h4>
         {inView ? (
-                <div ref={ref} className="w-full bg-gris-oscuro border-2 border-gris-intermedio rounded-full h-4">
-                    <div className='bg-gris-intermedio h-3 rounded-full transition-all delay-500 duration-1000' style={{'width': props.progress+"%"}}></div>
+                <div ref={ref} className="w-full bg-gris-intermedio border-2 border-gris-intermedio rounded-full h-4">
+                    <div className='bg-amarillo h-3 rounded-full transition-all delay-500 duration-1000' style={{'width': props.progress+"%"}}></div>
                 </div>
           ) : ( 
                 <div ref={ref} className="w-full bg-gris-intermedio border-2 border-gris-intermedio rounded-full h-4">
-                    <div className="bg-gris-intermedio h-3 rounded-full w-[0%]" /*style="width: $(progress)%"*/></div>
+                    <div className="bg-amarillo h-3 rounded-full w-[0%]" /*style="width: $(progress)%"*/></div>
                 </div>           
           )}
     </div>
