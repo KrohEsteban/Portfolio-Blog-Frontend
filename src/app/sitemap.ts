4e7f20530b8bc0049604adabab9af9dcf5d7e895
Esteban Kroh <krohesteban@gmail.com>
2023-06-03 17:16:49 -0300

    barra en blog sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -35,7 +35,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     blogs.docs.map((item: any) => {
        sitemap.push(
         {
-                url: 'https://estebankroh.com/blog'+item.Slug,
+                url: 'https://estebankroh.com/blog/'+item.Slug,
                 lastModified: new Date(), 
             }
        )
